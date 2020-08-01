40f503537a5ee27bedf1d7677fd43bf5991b10ca
Eileen Li <eileenlimur@gmail.com>
2020-08-01 18:43:44 -0400

    make component CategoryButton, add to stories

--- final-project-fe/src/App.js
+++ final-project-fe/src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Router, Route, Switch } from "react-router";
 import './index.css';
 
+// import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: blue,
+//   },
+// });
+
 function App() {
   return <div className="App">Hello World from Eileen, Keith, Sandeep</div>;
 }
---
+++ final-project-fe/src/components/CategoryButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+export default function CategoryButton(props) {
+  return (
+    <Button variant="contained" color="primary">{props.question}</Button>
+  )
+}
