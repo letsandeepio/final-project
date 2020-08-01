1535f8c205566285e61fd8fede7863a78fc2afd9
Eileen Li <eileenlimur@gmail.com>
2020-08-01 15:42:23 -0400

    moved index.css

--- final-project-fe/src/App.js
+++ final-project-fe/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router, Route, Switch } from "react-router";
-import '../public/index.css';
+import './index.css';
 
 function App() {
   return <div className="App">Hello World from Eileen, Keith, Sandeep</div>;
