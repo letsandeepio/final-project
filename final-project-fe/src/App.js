16e52a96d01d0792762733887eb5715b5da6a691
letsandeepio <sandeepchopra7@gmail.com>
2020-08-01 10:58:54 -0400

    :tada: initial commit

---
+++ final-project-fe/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <div className="App">Hello World from Eileen, Keith, Sandeep</div>;
+}
+
+export default App;
