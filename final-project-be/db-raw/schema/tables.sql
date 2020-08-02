594315e677f7ae4a002fc3077cf42ed0b2694d02
letsandeepio <sandeepchopra7@gmail.com>
2020-08-02 17:21:05 -0400

    minor fixes for the db seeds & schemas

--- final-project-be/db-raw/schema/tables.sql
+++ final-project-be/db-raw/schema/tables.sql
@@ -1,22 +1,20 @@
--- Drop and recreate Users table (Example)
+DROP TABLE IF EXISTS users CASCADE;
 
-DROP TABLE IF EXISTS users
-CASCADE;
-DROP TABLE IF EXISTS activities
-CASCADE;
+DROP TABLE IF EXISTS activities CASCADE;
 
-CREATE TABLE users(
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
+CREATE TABLE users (
+  id serial PRIMARY KEY NOT NULL,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE activities(
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  title VARCHAR(255) NOT NULL,
-  category VARCHAR(255) NOT NULL,
-  duration INTEGER NOT NULL,
-  completed BIT
+CREATE TABLE activities (
+  id serial PRIMARY KEY NOT NULL,
+  user_id integer REFERENCES users (id),
+  title varchar(255) NOT NULL,
+  category varchar(255) NOT NULL,
+  duration integer NOT NULL,
+  completed integer
 );
+
