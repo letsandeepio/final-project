ae4829e78a99d3989019103a678a4d29458b93a4
Eileen Li <eileenlimur@gmail.com>
2020-08-02 16:09:30 -0400

    add basic framework to CategoryDropdown

--- final-project-fe/src/components/CategoryDropdown.js
+++ final-project-fe/src/components/CategoryDropdown.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
 
 export default function(props) {
   return (
-    <div></div>
+    <div>
+      <FormControl>
+        <Select>
+          <MenuItem value="eat">What should I eat?</MenuItem>
+          <MenuItem value="do">What should I do?</MenuItem>
+          <MenuItem value="cook">What should I cook?</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
   )
 }
