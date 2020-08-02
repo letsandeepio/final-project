96263bb49bcbfc44fbf1f1907c2b285e647cd417
Eileen Li <eileenlimur@gmail.com>
2020-08-02 18:30:13 -0400

    add pluralize package to dependencies; add pluralize func to TimePicker to change minute and hour accordingly

--- final-project-fe/src/components/TimePicker.js
+++ final-project-fe/src/components/TimePicker.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
-import 
 import "../index.scss"
-
+import pluralize from "pluralize";
 
 export default function TimePicker(props) {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+
+  const hourSelection = pluralize("hour", hours);
+  const minuteSelection = pluralize("minute", minutes);
+
   return (
     <div className="time-picker-container">
       <form noValidate autoComplete="off" className = "time-picker-form">
         <section>I have&nbsp;</section>
-        <TextField value="2" onChange={props.onChange} size="2" maxlength="2"/>
-        <section>&nbsp;hours and&nbsp;</section>
-        <TextField value="30" onChange={props.onChange} size="2" maxlength="2"/>
-        <section>&nbsp;minutes</section>
+        <TextField value={hours} onChange={props.onChange} size="2" maxlength="2"/>
+        <section>&nbsp;{hourSelection} and&nbsp;</section>
+        <TextField value={minutes} onChange={props.onChange} size="2" maxlength="2"/>
+        <section>&nbsp;{minuteSelection}</section>
       </form>
     </div>
   )
 }
-
-//&nbsp
