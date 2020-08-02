f08e561b59af6e9a9e600e6ca6cf1065804a1596
Keith Millar <keithmillar@hotmail.ca>
2020-08-02 18:40:31 -0400

    fixed story so onClick would register properly

--- final-project-fe/src/stories/5-AddActivityButton.stories.js
+++ final-project-fe/src/stories/5-AddActivityButton.stories.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { linkTo } from '@storybook/addon-links';
+import { action } from '@storybook/addon-actions';
+
 import AddActivityButton from "../components/AddActivityButton";
 
 export default {
@@ -7,4 +9,4 @@ export default {
   component: AddActivityButton,
 };
 
-export const AddActivity = () => <AddActivityButton />;
+export const AddActivity = () => <AddActivityButton onClick={action('Add Activity Button Clicked!')}/>;
