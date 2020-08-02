9d22848071040ab6a4dc814a5f28234850e41d2d
Eileen Li <eileenlimur@gmail.com>
2020-08-02 18:40:07 -0400

    Create ability for SuggestionCard to accept activities as prop. Create a helpers folder with function timeConverter

--- final-project-fe/src/components/SuggestionCard.js
+++ final-project-fe/src/components/SuggestionCard.js
@@ -7,7 +7,7 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 275,
+    minWidth: 150,
     maxWidth: 280,
   },
   pos: {
@@ -15,17 +15,17 @@ const useStyles = makeStyles({
   },
 });
 
-export default function SimpleCard() {
+export default function SuggestionCard(props) {
   const classes = useStyles();
 
   return (
     <Card className={classes.root}>
       <CardContent>
       <Typography variant="h5" component="h2">
-        Activity
+        {props.activity.title}
       </Typography>
         <Typography color="textSecondary" className={classes.pos}>
-          3 hours
+        {props.activity.duration}
         </Typography>
       </CardContent>
     </Card>
--- final-project-fe/src/stories/8-SuggestionCard.stories.js
+++ final-project-fe/src/stories/8-SuggestionCard.stories.js
@@ -2,17 +2,11 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import SuggestionCard from '../components/SuggestionCard';
 
-const suggestions = [
-  {title: "Enchiladas", duration: 90},
-  {title: "Tacos", duration: 20},
-  {title: "Grilled Cheese", duration: 15},
-  {title: "Chicken Pot Pie", duration: 45},
-  {title: "BBQ", duration: 60},
-]
+const suggestion = {title: "Enchiladas", duration: 90}
 
 export default {
   title: 'SuggestionCard',
   component: SuggestionCard,
 };
 
-export const Text = () => <SuggestionCard onClick={action('clicked')}>Hello SuggestionCard</SuggestionCard>;
+export const Text = () => <SuggestionCard onClick={action('clicked')} activity={suggestion}>Hello SuggestionCard</SuggestionCard>;
