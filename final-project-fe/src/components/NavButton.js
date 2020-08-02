3300e8f7dd9fae8c5720e9fdc00e4eb1645661a2
Eileen Li <eileenlimur@gmail.com>
2020-08-02 00:01:31 -0400

    add story for NavButton, completed NavBtuton component

--- final-project-fe/src/components/NavButton.js
+++ final-project-fe/src/components/NavButton.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+
 
 export default function NavButton(props) {
-  
+  return (
+  <Button variant="contained" color="secondary">{props.children}</Button>
+  )
 }
