4ecdc17c58f93fe95d4ceb136fd415650a2194b9
Keith Millar <keithmillar@hotmail.ca>
2020-08-02 19:19:30 -0400

    added onclick events for category button list

--- final-project-fe/src/components/CategoryButtonList.js
+++ final-project-fe/src/components/CategoryButtonList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+import { action } from '@storybook/addon-actions';
 import CategoryButton from "../components/CategoryButton";
 
 export default function CategoryButtonList(props) {
@@ -8,7 +9,7 @@ export default function CategoryButtonList(props) {
     category => {
       return (
         <ul>
-          <CategoryButton>{category.question}</CategoryButton>
+          <CategoryButton onClick={action(`${category.question} - clicked`)}>{category.question}</CategoryButton>
         </ul>
       )}
   )
