1a68dbeab484bd09a793c7764c340e7edf4787c4
Keith Millar <keithmillar@hotmail.ca>
2020-08-03 19:00:45 -0400

    fixed some spacing issues and removed commented out code

--- final-project-fe/src/pages/HomePage.js
+++ final-project-fe/src/pages/HomePage.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import CategoryButtonList from '../components/CategoryButtonList';
 import GeneralBigButton from '../components/GeneralBigButton';
 import { Typography } from '@material-ui/core';
-
-
 
 
 export default function CategoryPage(props) {
@@ -14,20 +11,6 @@ export default function CategoryPage(props) {
         <Typography variant="h3">we can help</Typography>
         <GeneralBigButton>get started!</GeneralBigButton>
       </div>
-
-
-
-
     </section>
   )
 }
-
-
-// return (
-//   <div className="homePage">
-//     <Typography variant="h1">Don't know what to do?</Typography>
-//     <Typography variant="h2">Just ask!</Typography>
-//     <TimePicker className="timePicker" onChange={props.onChange}/>
-//     <CategoryButtonList className="CategoryButtonList" categories={props.categories} onChange={props.onChange}/>
-//   </div>
-// )
