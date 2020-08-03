d567aa3c9d5efcced3cd8d3c9fb66ffee2f5e90c
Eileen Li <eileenlimur@gmail.com>
2020-08-03 11:58:24 -0400

    add route for categorypage

--- final-project-fe/src/App.js
+++ final-project-fe/src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React from "react";
 // import { Router, Route, Switch } from 'react-router';
 import Login from "./components/Login";
 import About from "./components/About";
 import Signup from "./components/Signup";
-import Header from './components/Header';
-
+import Header from "./components/Header";
+import CategoryPage from "./CategoryPage";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import './index.scss';
+import "./index.scss";
+
+const categories = [
+  {question: "what should i do?"},
+  {question: "what should i watch?"},
+  {question: "where should i eat?"},
+  {question: "what should i cook?"},
+  {question: "what else could i do?"},
+]
 
 function App() {
-  return (<div>
-    <Header />
-    <Switch>
-      <Route path="/login" component={Login} exact />
-      <Route path="/signup" component={Signup} exact />
-      <Route path="/about" component={About} exact />
-    </Switch>
-      </div>)
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/login" component={Login} exact />
+        <Route path="/signup" component={Signup} exact />
+        <Route path="/about" component={About} exact />
+        <Route exact path="/categories">
+          <CategoryPage categories={categories}/>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
