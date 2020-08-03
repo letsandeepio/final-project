d96fc1a4fd083b4b580342ddcfe98f54438397a4
Eileen Li <eileenlimur@gmail.com>
2020-08-03 11:21:56 -0400

    add CategpryPage story and complete component and style it

---
+++ final-project-fe/src/stories/10-CategoryPage.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CategoryPage from '../CategoryPage';
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'CategoryPage',
+  component: CategoryPage,
+};
+
+const categoryArray = [
+  {question: "what should i do?"},
+  {question: "what should i watch?"},
+  {question: "where should i eat?"},
+  {question: "what should i cook?"},
+  {question: "what else could i do?"},
+]
+
+export const Default = () => <CategoryPage categories={categoryArray} onChange={action('changed')}/>;
