3b7f87b6ad6ad54adff2ebd930c287b307a8b23a
letsandeepio <sandeepchopra7@gmail.com>
2020-08-03 12:26:19 -0400

    adds login component

---
+++ final-project-fe/src/stories/30 - Login.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Login from '../components/Login';
+
+export default {
+  title: 'Login Screen',
+  component: Login
+};
+
+export const LoginScreen = () => <Login />;
