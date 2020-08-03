dffcb5214befdb18531c2abb694a5d4b3782fb8b
Eileen Li <eileenlimur@gmail.com>
2020-08-03 13:38:14 -0400

    rename timeInputFormatter helper func to timeInputFormat, use it in TimePicker component

--- final-project-fe/src/components/TimePicker.js
+++ final-project-fe/src/components/TimePicker.js
@@ -2,43 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { Typography, TextField } from '@material-ui/core';
 import "../index.scss"
 import Pluralize from "react-pluralize";
+import timeInputFormat from '../helpers/timeInputFormat';
 
 export default function TimePicker(props) {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
 
-  const updateHours = function(value) {
-    let newHour = Number(value);
-    
-    if (isNaN(newHour)) {
-      newHour = value.slice(0, value.length - 1);
-    }
-
-    if (value.length > 2) {
-      newHour = value.slice(0,2);
-    }
-    setHours(newHour);
-  }
-  const updateMinutes = function(value) {
-    let newMinutes = Number(value);
-    
-    if (isNaN(newMinutes)) {
-      newMinutes = value.slice(0, value.length - 1);
-    }
-
-    if (value.length > 2) {
-      newMinutes = value.slice(0,2);
-    }
-    setMinutes(newMinutes);
-  }
+  console.log(hours, minutes);
 
   return (
     <div className="timePicker">
       <form noValidate autoComplete="off" className = "time-picker-form">
         <Typography variant="h2">I have&nbsp;</Typography>
-        <TextField placeholder={0} value={hours} onChange={e=>updateHours(e.target.value)} autoFocus/>
+        <TextField placeholder={0} value={hours} onChange={e=>setHours(timeInputFormat(e.target.value))} autoFocus/>
         <Typography variant="h2">&nbsp;<Pluralize singular={'hour'} count={hours} showCount={false}/> and&nbsp;</Typography>
-        <TextField placeholder={0} value={minutes} onChange={e=>updateMinutes(e.target.value)} />
+        <TextField placeholder={0} value={minutes} onChange={e=>setMinutes(timeInputFormat(e.target.value))} />
         <Typography variant="h2">&nbsp;<Pluralize singular={'minute'} count={minutes} showCount={false}/></Typography>
       </form>
     </div>
