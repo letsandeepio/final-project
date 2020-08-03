c357b7b795fa0aa188bc0384c04825ea21f67935
Eileen Li <eileenlimur@gmail.com>
2020-08-03 14:37:31 -0400

    use prettier on TimePicker

--- final-project-fe/src/components/TimePicker.js
+++ final-project-fe/src/components/TimePicker.js
@@ -1,24 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, TextField } from '@material-ui/core';
-import "../index.scss"
+import React, { useState, useEffect } from "react";
+import { Typography, TextField } from "@material-ui/core";
+import "../index.scss";
 import Pluralize from "react-pluralize";
-import timeInputFormat from '../helpers/timeInputFormat';
+import timeInputFormat from "../helpers/timeInputFormat";
 
 export default function TimePicker(props) {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
 
-  console.log(hours, minutes);
-
   return (
     <div className="timePicker">
-      <form noValidate autoComplete="off" className = "time-picker-form">
+      <form noValidate autoComplete="off" className="time-picker-form">
         <Typography variant="h2">I have&nbsp;</Typography>
-        <TextField placeholder={0} value={hours} onChange={e=>setHours(timeInputFormat(e.target.value))} autoFocus/>
-        <Typography variant="h2">&nbsp;<Pluralize singular={'hour'} count={hours} showCount={false}/> and&nbsp;</Typography>
-        <TextField placeholder={0} value={minutes} onChange={e=>setMinutes(timeInputFormat(e.target.value))} />
-        <Typography variant="h2">&nbsp;<Pluralize singular={'minute'} count={minutes} showCount={false}/></Typography>
+        <TextField
+          placeholder={0}
+          value={hours}
+          onChange={(e) => setHours(timeInputFormat(e.target.value))}
+          autoFocus
+        />
+        <Typography variant="h2">
+          &nbsp;
+          <Pluralize singular={"hour"} count={hours} showCount={false} />{" "}
+          and&nbsp;
+        </Typography>
+        <TextField
+          placeholder={0}
+          value={minutes}
+          onChange={(e) => setMinutes(timeInputFormat(e.target.value))}
+        />
+        <Typography variant="h2">
+          &nbsp;
+          <Pluralize singular={"minute"} count={minutes} showCount={false} />
+        </Typography>
       </form>
     </div>
-  )
+  );
 }
