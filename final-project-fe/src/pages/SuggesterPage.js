fcf3839c63e8e49282c28b0bef951f4efad39a46
Eileen Li <eileenlimur@gmail.com>
2020-08-04 11:22:40 -0400

    implement routes for suggesterpage

--- final-project-fe/src/pages/SuggesterPage.js
+++ final-project-fe/src/pages/SuggesterPage.js
@@ -5,15 +5,18 @@ import SuggestionCard from '../components/SuggestionCard'
 import TimePicker from '../components/TimePicker'
 import SuggesterButtonBox from '../components/SuggesterButtonBox'
 
+import {useHistory } from 'react-router-dom';
+
 const suggestion = {title: "Enchiladas", duration: 90}
 
 export default function SuggesterPage(props) {
+  let history = useHistory();
   return (
     <div className="suggestorPage">
       <CategoryDropdown questions={props.categories} question={props.category}/>
       <TimePicker onChange={props.onTimeChange} timeAvailable={props.timeAvailable}/>
       <SuggestionCard activity={suggestion}/>
-      <SuggesterButtonBox onAccept={e => console.log("Accepted " + e)} onReject={e => console.log("Rejected " + e)}/>
+      <SuggesterButtonBox onAccept={()=>history.push('/success')} onReject={e => console.log("Rejected " + e)}/>
     </div>
   )
 }
