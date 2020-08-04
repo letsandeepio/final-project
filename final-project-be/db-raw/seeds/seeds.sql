7534d3790d6e57e0b219dfd15d61102603024b2d
Eileen Li <eileenlimur@gmail.com>
2020-08-04 16:23:57 -0400

    put seed value add values into multiple lines

--- final-project-be/db-raw/seeds/seeds.sql
+++ final-project-be/db-raw/seeds/seeds.sql
@@ -2,4 +2,9 @@ INSERT INTO users(name, email, password)
   VALUES ('Sandeep', 'sandeepchopra7@gmail.com', 'password'), ('Iva Harrison', 'allisonjackson@mail.com', 'password');
 
 INSERT INTO activity(user_id, title, category, duration, completed)
-  VALUES (1, 'Shark', 'watch', '120', 0), (1, 'Superman', 'watch', '130', 0), (1, 'Mcdonalds', 'eat', '30', 0), (1, 'Popeyes', 'eat', '30', 1), (1, 'Burrate Chicken', 'cook', '100', 0), (1, 'Laundry', 'others', '15', 0);
+  VALUES (1, 'Shark', 'watch', '120', 0),
+  (1, 'Superman', 'watch', '130', 0),
+  (1, 'Mcdonalds', 'eat', '30', 0),
+  (1, 'Popeyes', 'eat', '30', 1),
+  (1, 'Burrate Chicken', 'cook', '100', 0),
+  (1, 'Laundry', 'others', '15', 0);
