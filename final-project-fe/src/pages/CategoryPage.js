eead3d1e960435038a1e37b15969b23ec8d99d73
Keith Millar <keithmillar@hotmail.ca>
2020-08-04 12:35:05 -0400

    added addactivity button to categories page, doesn't yet do anything

--- final-project-fe/src/pages/CategoryPage.js
+++ final-project-fe/src/pages/CategoryPage.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import CategoryButtonList from '../components/CategoryButtonList';
 import TimePicker from '../components/TimePicker';
+import AddActivityButton from '../components/AddActivityButton';
 import { Typography } from '@material-ui/core';
 
 export default function CategoryPage(props) {
   
   return (
-    <div className="categoryPage">
-      <Typography variant="h1">Don't know what to do?</Typography>
-      <Typography variant="h2">Just ask!</Typography>
-      <TimePicker className="timePicker" onChange={props.onTimeChange} timeAvailable={props.timeAvailable}/>
-      <CategoryButtonList className="CategoryButtonList" categories={props.categories} onSelect={props.onSelect}/>
+    <div>
+      <div className="categoryPage">
+        <Typography variant="h1">Don't know what to do?</Typography>
+        <Typography variant="h2">Just ask!</Typography>
+        <TimePicker className="timePicker" onChange={props.onTimeChange} timeAvailable={props.timeAvailable}/>
+        <CategoryButtonList className="CategoryButtonList" categories={props.categories} onSelect={props.onSelect}/>
+      </div>
+        <AddActivityButton className="addActivityButton"></AddActivityButton>
     </div>
   )
 }
