a6e73b8be8d5abcd8cc5625942ccd9360118e067
Eileen Li <eileenlimur@gmail.com>
2020-08-05 11:45:11 -0400

    attempts on activities query

--- final-project-be/src/resolvers/Query.ts
+++ final-project-be/src/resolvers/Query.ts
@@ -1,8 +1,11 @@
+// import { getUserId } from "../helpers";
+
 function info() {
   return `Eileen, Keith and Sandeep are setting up Authentication!`;
 }
 
 async function activities(parent: any, args: any, context: any) {
+  // const userId = getUserId(context);
   const activities = await context.prisma.activity.findMany({
     where: { user_id: args.userId}
   })
