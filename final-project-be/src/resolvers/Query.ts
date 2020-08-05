c46efbc065fb0db2698e427d1498545eb117579d
Eileen Li <eileenlimur@gmail.com>
2020-08-05 12:40:53 -0400

    add Context as type to context variable in acitivities resolver, bugs exist (getUserId) not working

--- final-project-be/src/resolvers/Query.ts
+++ final-project-be/src/resolvers/Query.ts
@@ -1,10 +1,11 @@
 import { getUserId } from "../helpers";
+import { Context } from '../index';
 
 function info() {
   return `Eileen, Keith and Sandeep are setting up Authentication!`;
 }
 
-async function activities(parent: any, args: any, context: any) {
+async function activities(parent: any, args: any, context: Context) {
   const userId = getUserId(context);
   const activities = await context.prisma.activity.findMany({
     where: { user_id: userId}
