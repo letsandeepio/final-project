c7be8372a936fae2e601bc43e9840ad4f6f0741d
Eileen Li <eileenlimur@gmail.com>
2020-08-07 20:01:53 -0400

    replace all hardcoded question arrays with reference to the one in constants.js

--- final-project-fe/src/stories/10-AddActivityForm.stories.js
+++ final-project-fe/src/stories/10-AddActivityForm.stories.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import AddActivityForm from "../components/AddActivityForm";
+import questions from '../constants';
 
 export default {
   title: 'AddActivityForm',
   component: AddActivityForm,
 };
 
-const CategoryButtons = [
-  {question: "what should i do?"},
-  {question: "what should i watch?"},
-  {question: "where should i eat?"},
-  {question: "what should i cook?"},
-  {question: "what else could i do?"},
-]
-
-export const AddActivity = () => <AddActivityForm questions={CategoryButtons} onChange={action('changed')} onClick={action('clicked')}></AddActivityForm>;
+export const AddActivity = () => <AddActivityForm questions={questions} onChange={action('changed')} onClick={action('clicked')}></AddActivityForm>;
--- final-project-fe/src/stories/4-CategoryDropdown.stories.js
+++ final-project-fe/src/stories/4-CategoryDropdown.stories.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import CategoryDropdown from '../components/CategoryDropdown';
+import questions from '../constants';
 
 export default {
   title: 'CategoryDropdown',
   component: CategoryDropdown,
 };
 
-const CategoryButtons = [
-  {question: "what should i do?"},
-  {question: "what should i watch?"},
-  {question: "where should i eat?"},
-  {question: "what should i cook?"},
-  {question: "what else could i do?"},
-]
-
-export const Dropdown = () => <CategoryDropdown questions={CategoryButtons} onChange={action('changed')}/>;
+export const Dropdown = () => <CategoryDropdown questions={questions} onChange={action('changed')}/>;
