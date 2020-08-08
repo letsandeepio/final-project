6805d750f7b481292b16e0a4b81c4a1ef639f23a
Keith Millar <keithmillar@hotmail.ca>
2020-08-07 23:01:38 -0400

    minor timepicker font tweak

--- final-project-fe/src/components/TimePicker.js
+++ final-project-fe/src/components/TimePicker.js
@@ -15,13 +15,13 @@ export default function TimePicker(props) {
   return (
     <div className="timePicker">
       <form noValidate autoComplete="off" className="time-picker-form">
-        <Typography variant="h2">I have&nbsp;</Typography>
+        <Typography>I have&nbsp;</Typography>
         <TextField
           value={timeAvailable.hours}
           onChange={(e)=>handleTimeChange({hours: timeInputFormat(e.target.value, 'hours'), minutes: timeAvailable.minutes})}
           autoFocus
         />
-        <Typography variant="h2">
+        <Typography>
           &nbsp;
           {pluralize('hour', timeAvailable.hours)}&nbsp;
           and&nbsp;
@@ -30,7 +30,7 @@ export default function TimePicker(props) {
           value={timeAvailable.minutes}
           onChange={(e)=>handleTimeChange({hours: timeAvailable.hours, minutes: timeInputFormat(e.target.value, 'minutes')})}
         />
-        <Typography variant="h2">
+        <Typography>
           &nbsp;
           {pluralize('minute', timeAvailable.minutes)}
         </Typography>
