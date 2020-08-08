241ccb12b385b8f278f5c92c982a841f7ae3a2e9
Keith Millar <keithmillar@hotmail.ca>
2020-08-07 20:12:09 -0400

    focused on the buttons, material ui is stubborn, commiting this before breaking it again

--- final-project-fe/src/components/CategoryButton.js
+++ final-project-fe/src/components/CategoryButton.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fff"
+  },
+}});
+
 export default function CategoryButton(props) {
   return (
-    <Button variant="contained" onClick={props.onClick} color="primary">{props.children}</Button>
+    <MuiThemeProvider theme={theme}>
+      <Button variant="contained" onClick={props.onClick} color="primary" fullWidth="true" style={{ fontFamily: 'Fredoka One', fontSize: '1.2em' }}>
+        {props.children}
+      </Button>
+    </MuiThemeProvider>
   )
 }
+
+// <MuiThemeProvider theme={theme}>
+// <Button variant="contained" onClick={props.onClick} color={primary} fullWidth="true">
+//   {props.children}
+// </Button>
+// </MuiThemeProvider>
+
+{/* <Button variant="contained" onClick={props.onClick} color="primary" fullWidth="true">
+{props.children}
+</Button> */}
--- final-project-fe/src/theme.js
+++ final-project-fe/src/theme.js
@@ -10,8 +10,40 @@ export const theme = createMuiTheme({
     },
     secondary: {
       light: '#ff7961',
-      main: '#2979ff',
-      dark: '#ba000d',
+      main: '#fff',
+      dark: '#f1f1f1',
+      contrastText: '#000'
+    }
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+
+});
+
+export const buttontheme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#e91e63',
+      dark: '#002884',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#fff',
+      dark: '#f1f1f1',
       contrastText: '#000'
     }
   }
