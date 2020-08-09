ecf2b85bca11618cd87a7b2ae58bc0f0bce8515f
Keith Millar <keithmillar@hotmail.ca>
2020-08-09 08:17:05 -0400

    homepage fixes, navbar overlap, fonts, button next

--- final-project-fe/src/pages/HomePage.js
+++ final-project-fe/src/pages/HomePage.js
@@ -6,14 +6,16 @@ import { Link } from 'react-router-dom';
 
 export default function CategoryPage(props) {
   return (
-    <section>
-      <div className="homePage">
-        <Typography variant="h2">don't know what to do?</Typography>
-        <Typography variant="h3">we can help</Typography>
-        <Button variant="contained" size="large" color="primary" component={Link} to="/categories" >
-          get started!
-        </Button>
-      </div>
+    <section className="move-down">
+      {/* <div className="homePage center-text"> */}
+        <div className="top-text homePage">
+          <Typography variant="h3">don't know what to do?</Typography>
+          <Typography variant="h1">we can help!</Typography>
+          <Button variant="contained" size="large" color="primary" component={Link} to="/categories" >
+            get started!
+          </Button>
+        </div>
+      {/* </div> */}
     </section>
   )
 }
