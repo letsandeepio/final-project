b63630c1f0b84003b6be3cae08a9de430a9c0df1
Eileen Li <eileenlimur@gmail.com>
2020-08-10 18:00:41 -0400

    fix typo

--- final-project-fe/src/components/AddActivityForm.js
+++ final-project-fe/src/components/AddActivityForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { useBeforeUnload } from 'react-beforeunload';
+import { useBeforeunload } from 'react-beforeunload';
 
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
@@ -95,7 +95,7 @@ export default function AddActivityForm(props) {
     }
   }, [data1]);
 
-  useBeforeUnload(()=> {
+  useBeforeunload(()=> {
     localStorage.removeItem('category');
     localStorage.removeItem('title');
     localStorage.removeItem('url');
