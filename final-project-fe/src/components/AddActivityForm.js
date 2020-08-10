ff51292413aeac897935f35eae994a3acea22470
Eileen Li <eileenlimur@gmail.com>
2020-08-10 18:00:14 -0400

    add useBeforeunload to dependencies. It allows us to clear form fields after leaving addformactivitypage

--- final-project-fe/src/components/AddActivityForm.js
+++ final-project-fe/src/components/AddActivityForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import ls from 'local-storage'
+import { useBeforeUnload } from 'react-beforeunload';
 
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
@@ -94,6 +94,14 @@ export default function AddActivityForm(props) {
       setUrl(data1.images[0]);
     }
   }, [data1]);
+
+  useBeforeUnload(()=> {
+    localStorage.removeItem('category');
+    localStorage.removeItem('title');
+    localStorage.removeItem('url');
+    localStorage.removeItem('hours');
+    localStorage.removeItem('minutes');
+  })
 
   function getImage() {
     if (data1 && data1.images) {
