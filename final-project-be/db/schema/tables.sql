38528abdc349f02f39a5c4a698bad36b43de71a6
letsandeepio <sandeepchopra7@gmail.com>
2020-08-10 11:02:35 -0400

    adds compelted_on to db and backend resolver

--- final-project-be/db/schema/tables.sql
+++ final-project-be/db/schema/tables.sql
@@ -3,18 +3,21 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS activity CASCADE;
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
+  id serial PRIMARY KEY NOT NULL,
+  name varchar(255) NOT NULL,
+  email varchar(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE activity (
   id serial PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users (id),
-  title VARCHAR(255) NOT NULL,
-  category VARCHAR(255) NOT NULL,
-  duration INTEGER NOT NULL,
-  status VARCHAR(255) DEFAULT 'incomplete',
-  image_url VARCHAR(255)
+  user_id integer REFERENCES users (id),
+  title varchar(255) NOT NULL,
+  category varchar(255) NOT NULL,
+  duration integer NOT NULL,
+  status varchar(255) DEFAULT 'incomplete',
+  image_url varchar(255),
+  completed_on varchar(255),
+  expiry_date varchar(255)
 );
+
