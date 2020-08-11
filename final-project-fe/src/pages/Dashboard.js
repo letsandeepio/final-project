5f5ccb968fc51e27a6987401feba61b5e281d4ff
letsandeepio <sandeepchopra7@gmail.com>
2020-08-11 12:49:40 -0400

    fixes overlap text

--- final-project-fe/src/pages/Dashboard.js
+++ final-project-fe/src/pages/Dashboard.js
@@ -24,94 +24,93 @@ export default function Dashboard() {
     stats = JSON.parse(data.getStats);
   }
 
-  console.log(stats);
-
   return stats ? (
     <section>
-      <div className="homePage move-down8">
-        <Typography variant="h3">All of your stats in one place!</Typography>
-        <Typography variant="h1">Dashboard</Typography>
-        <Container maxWidth="sm" style={{ marginTop: '3em' }}>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              transform: 'scale(0.7)'
-            }}
-          >
-            <div>
-              <Typography variant="h2" style={{ color: '#afafaf' }}>
-                Pending Activities
-              </Typography>
-              <Typography variant="h1">
-                {stats.incompleteActivityCount}
-              </Typography>
-            </div>
+      <div>
+        <div className="homePage move-down8">
+          <Typography variant="h3">All of your stats in one place!</Typography>
+          <Typography variant="h1">Dashboard</Typography>
+          <Container maxWidth="sm" style={{ marginTop: '3em' }}>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-evenly'
+              }}
+            >
+              <div>
+                <Typography variant="h2" style={{ color: '#afafaf' }}>
+                  Pending Activities
+                </Typography>
+                <Typography variant="h1">
+                  {stats.incompleteActivityCount}
+                </Typography>
+              </div>
 
-            <div>
-              <Typography variant="h2" style={{ color: '#afafaf' }}>
-                Complete Activities
-              </Typography>
-              <Typography variant="h1">
-                {stats.completeActivityCount}
-              </Typography>
+              <div>
+                <Typography variant="h2" style={{ color: '#afafaf' }}>
+                  Complete Activities
+                </Typography>
+                <Typography variant="h1">
+                  {stats.completeActivityCount}
+                </Typography>
+              </div>
             </div>
-          </div>
-          <Card className="graph-card">
-            <CardContent>
-              <div>
-                <BarChart data={stats.monthlyBarChart} />
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'center'
-                  }}
-                >
-                  <Typography variant="h3">
-                    Completed Activities by Month
-                  </Typography>
+            <Card className="graph-card">
+              <CardContent>
+                <div>
+                  <BarChart data={stats.monthlyBarChart} />
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center'
+                    }}
+                  >
+                    <Typography variant="h3">
+                      Completed Activities by Month
+                    </Typography>
+                  </div>
                 </div>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className="graph-card">
-            <CardContent>
-              <div>
-                <PieChart data={stats.inCompleteCategoryBreakdown} />
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'center'
-                  }}
-                >
-                  <Typography variant="h3">
-                    Incomplete Activities by Category
-                  </Typography>
+              </CardContent>
+            </Card>
+            <Card className="graph-card">
+              <CardContent>
+                <div>
+                  <PieChart data={stats.inCompleteCategoryBreakdown} />
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center'
+                    }}
+                  >
+                    <Typography variant="h3">
+                      Incomplete Activities by Category
+                    </Typography>
+                  </div>
                 </div>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className="graph-card">
-            <CardContent>
-              <div>
-                <PieChart data={stats.completeCategoryBreakdown} />
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    marginBottom: '30px'
-                  }}
-                >
-                  <Typography variant="h3">
-                    Complete Activities by Category
-                  </Typography>
+              </CardContent>
+            </Card>
+            <Card className="graph-card">
+              <CardContent>
+                <div>
+                  <PieChart data={stats.completeCategoryBreakdown} />
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      marginBottom: '30px'
+                    }}
+                  >
+                    <Typography variant="h3">
+                      Complete Activities by Category
+                    </Typography>
+                  </div>
                 </div>
-              </div>
-            </CardContent>
-          </Card>
-        </Container>
+              </CardContent>
+            </Card>
+          </Container>
+        </div>
+        <div style={{ height: '8em' }}></div>
       </div>
-      <div style={{ height: '8em' }}></div>
     </section>
   ) : (
     'Loading stats...'
