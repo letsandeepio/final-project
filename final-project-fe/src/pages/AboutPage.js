73fdb98ab858c4bec64b67894eabf6569f8cb978
Keith Millar <keithmillar@hotmail.ca>
2020-08-13 00:01:52 -0400

    about page first pass

---
+++ final-project-fe/src/pages/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AboutPage() {
+  return (
+    <section className='homePage'>
+      <img src="./images/slide-logo.png"></img>
+      <img src="./images/slide-tech-stack.png"></img>
+    </section>
+  );
+}
